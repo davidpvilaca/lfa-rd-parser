e58c2fa8b4ac135da47ea20532a9238f3cc78cb7
David Vilaça <vilacapdavid@gmail.com>
2019-05-01 11:39:08 -0300

    wip: calculate result

--- src/parser/RDParserMEL.java
+++ src/parser/RDParserMEL.java
@@ -1,8 +1,41 @@
 package parser;
 
-import java.util.Arrays;
+import org.jetbrains.annotations.Contract;
+
+import java.util.*;
+
+enum ETypeSymbol {
+    OPERATOR,
+    NUMBER,
+    DOT,
+    EULER,
+    PAREN_L,
+    PAREN_R
+}
+
+class Symbol {
+    public String token;
+    public final ETypeSymbol type;
+
+    public Symbol(Character token, ETypeSymbol type) {
+        this.type = type;
+        this.token = "";
+        this.token += token;
+    }
+
+    public Symbol(String token, ETypeSymbol type) {
+        this.type = type;
+        this.token = token;
+    }
+
+    public void add(Character c) {
+        this.token += c;
+    }
+}
 
 public class RDParserMEL extends Parser {
+
+    private ArrayList<Symbol> processList = new ArrayList<>();
 
     public RDParserMEL() {
         super();
@@ -15,6 +48,7 @@ public class RDParserMEL extends Parser {
         if (this.symbol != null) {
             throw new Error("Not accept tokens");
         }
+        this.process();
     }
 
     public void expr() {
@@ -30,7 +64,8 @@ public class RDParserMEL extends Parser {
     private void manyTerm() {
         while (true) {
             try {
-                if (this.expect('+') || this.expect('-')) {
+                if (this.expectSumSub()) {
+                    this.putSymbol(ETypeSymbol.OPERATOR);
                     this.next();
                     this.term();
                 } else {
@@ -45,6 +80,7 @@ public class RDParserMEL extends Parser {
     public void factor() {
         this.base();
         if (this.expect('^')) {
+            this.putSymbol(ETypeSymbol.OPERATOR);
             this.next();
             this.factor();
         }
@@ -55,8 +91,10 @@ public class RDParserMEL extends Parser {
         while (true) {
             try {
                 if (Arrays.asList(terms).contains(this.symbol)) {
+                    this.putSymbol(ETypeSymbol.OPERATOR);
                     this.next();
                     if (this.expect('/')) {
+                        this.putSymbol(ETypeSymbol.OPERATOR);
                         this.next();
                     }
                     this.factor();
@@ -70,7 +108,8 @@ public class RDParserMEL extends Parser {
     }
 
     public void base() {
-        if (this.expect('+') || this.expect('-')) {
+        if (this.expectSumSub()) {
+            this.putSymbol(ETypeSymbol.OPERATOR);
             this.next();
             this.base();
             return;
@@ -80,9 +119,11 @@ public class RDParserMEL extends Parser {
             return;
         } catch (Error e) { }
         if (this.expect('(')) {
+            this.putSymbol(ETypeSymbol.PAREN_L);
             this.next();
             this.expr();
             if (this.expect(')')) {
+                this.putSymbol(ETypeSymbol.PAREN_R);
                 this.next();
                 return;
             }
@@ -94,12 +135,15 @@ public class RDParserMEL extends Parser {
         this.digit();
         this.manyDigits();
         if (this.expect('.')) {
+            this.putSymbol(ETypeSymbol.DOT);
             this.next();
             this.manyDigits();
         }
         if (this.expect('E') || this.expect('e')) {
+            this.putSymbol(ETypeSymbol.EULER);
             this.next();
-            if (this.expect('+') || this.expect('-')) {
+            if (this.expectSumSub()) {
+                this.putSymbol(ETypeSymbol.OPERATOR);
                 this.next();
             }
             this.digit();
@@ -111,6 +155,7 @@ public class RDParserMEL extends Parser {
         Character[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
         boolean contains = Arrays.asList(digits).contains(this.symbol);
         if (contains) {
+            this.putSymbol(ETypeSymbol.NUMBER);
             this.next();
         } else {
             throw new Error("Invalid digit");
@@ -125,6 +170,66 @@ public class RDParserMEL extends Parser {
                 break;
             }
         }
+    }
+
+    private boolean expectSumSub() {
+        return this.expect('+') || this.expect('-');
+    }
+
+    private void putSymbol(ETypeSymbol type) {
+        Symbol s = this.processList.isEmpty() ? null : this.processList.get(this.processList.size() - 1);
+        boolean sIsNumber = s != null && (s.type == ETypeSymbol.NUMBER || s.type == ETypeSymbol.EULER);
+        boolean symbolIsNumber = type == ETypeSymbol.NUMBER || type == ETypeSymbol.EULER || type == ETypeSymbol.DOT;
+        if (sIsNumber && symbolIsNumber) {
+            s.add(this.symbol);
+        } else {
+            this.processList.add(new Symbol(this.symbol, type));
+        }
+    }
+
+    private double process() {
+        double result = 0;
+        Stack<Symbol> stack = this.getStack();
+        Symbol n1, n2, op;
+
+        while (stack.size() > 1) {
+            n1 = stack.pop();
+            op = stack.pop();
+            n2 = stack.pop();
+            result += this.calc(n1.token, op.token.charAt(0), n2.token);
+        }
+
+        return result;
+    }
+
+    private double calc(String n1, Character operator, String n2) {
+        double _n1 = Double.parseDouble(n1);
+        double _n2 = Double.parseDouble(n2);
+        switch (operator) {
+            case '^':
+                return Math.pow(_n1, _n2);
+            case '*':
+                return _n1 * _n2;
+            case '/':
+                return _n1 / _n2;
+            case '%':
+                return _n1 % _n2;
+            case '+':
+                return _n1 + _n2;
+            case '-':
+                return _n1 - _n2;
+            default:
+                throw new Error("Error on calc: Operator not valid");
+        }
+    }
+
+    private Stack<Symbol> getStack() {
+        Character[] maxPrecedence = { '^' };
+        Character[] midPrecedence = { '*', '/', '%' };
+        Character[] lowPrecedence = { '+', '-' };
+        Stack<Symbol> stack = new Stack<>();
+
+        return stack;
     }
 
 }
