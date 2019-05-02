b4c09c16c2bd98de6a269cb0c4b9aaf94c5886b5
David Vilaça <vilacapdavid@gmail.com>
2019-05-02 16:09:16 -0300

    feat: input tokens scan

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 
 import parser.RDParserMEL;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String tokens = "10 * 5 + 100 / 10 - 5 + 7 % 2";
+        String tokens = new Scanner(System.in).nextLine(); // Ex: "10 * 5 + 100 / 10 - 5 + 7 % 2"
         RDParserMEL parser = new RDParserMEL();
         double result = parser.parse(tokens);
-        System.out.println("\"" + tokens + "\" > Accepted");
-        System.out.println("Result: " + result);
+        System.out.println(result);
+
     }
 
 }
